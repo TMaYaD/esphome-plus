b77fa5b01813d15f480c365e4f974eaf949db580
Subhash Chandra <TMaYaD@LoonyB.in>
2023-10-29 19:04:26 +0530

    feat: reconsile device config
    reads the existing config from [backup component](https://github.com/dentra/esphome-components/tree/master/components/backup)

--- esphome_plus/__init__.py
+++ esphome_plus/__init__.py
@@ -6,6 +6,11 @@ import click
 def cli():
     pass
 
+
+from .reconcile import reconcile
+
+cli.add_command(reconcile)
+
 from .upstream import add_upsteam_commands_to
 
 add_upsteam_commands_to(cli)
